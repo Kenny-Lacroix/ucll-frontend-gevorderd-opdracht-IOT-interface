ec4b72ba3221049abf64220a6787216029f66870
Kenny-Lacroix <kenny.lacroix@telenet.be>
2021-12-07 22:41:19 +0100

    updated folder structure

--- Student/Server/server.js
+++ Student/Server/server.js
@@ -4,18 +4,10 @@ const APP = X()
 const PORT = 1999
 const PATH = require('path');
 
-/* ---DEMO DATA--- */
-const iotData = require("./iotData")
-// import { iotData } from './iotData.js';
-
 /* ---EXPRESS RELATED CODE--- */
 APP.use('/iotDashboard', X.static(PATH.join(__dirname, '../Client/Public/iotDashboard')));
 
 /* ---ENDPOINTS--- */
-APP.get('/iotDashboard/sensorData', (req, res) => {
-    return res.send(JSON.stringify(testData));
-});
-
 APP.post('/iotDashboard/sensorStatus', (req, res) => {
     return res.send('Received a POST HTTP method');
 });
